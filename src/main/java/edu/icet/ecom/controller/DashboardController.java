c425ba00d1b286d72ca549abe9fe32bb55cd2fcc
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-06-26 05:19:20 +0530

    Create DashboardController.java

---
+++ src/main/java/edu/icet/ecom/controller/DashboardController.java
@@ -0,0 +1,24 @@
+package edu.icet.ecom.controller;
+
+import edu.icet.ecom.model.dto.DashboardAnalyticsDto;
+import edu.icet.ecom.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dashboard")
+@RequiredArgsConstructor
+@CrossOrigin()
+public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    @GetMapping("/analytics")
+    public ResponseEntity<DashboardAnalyticsDto> getAnalytics() {
+        return ResponseEntity.ok(dashboardService.getDashboardAnalytics());
+    }
+}
