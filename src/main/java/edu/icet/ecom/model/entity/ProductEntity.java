3321673362fa575e5007c70ed387bbc97c48f943
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-06-26 05:19:47 +0530

    Update ProductEntity.java

--- src/main/java/edu/icet/ecom/model/entity/ProductEntity.java
+++ src/main/java/edu/icet/ecom/model/entity/ProductEntity.java
@@ -2,6 +2,7 @@ package edu.icet.ecom.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import edu.icet.ecom.enums.StockStatus;
+import edu.icet.ecom.enums.ProductUnit;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,6 +26,9 @@ public class ProductEntity {
     @Enumerated(EnumType.STRING)
     private StockStatus stockStatus;
 
+    @Enumerated(EnumType.STRING)
+    private ProductUnit unit;
+
     private String currency;
     private Double discountPercentage;
     private String material;
@@ -32,7 +36,11 @@ public class ProductEntity {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+    private SupplierEntity supplier;
+
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
-    @JsonIgnoreProperties("product") // Prevents variants from re-loading this product object
+    @JsonIgnoreProperties("product")
     private List<ProductVariantEntity> variants;
 }
