dfd416b51c99f901e8aa0736866c5e61fa3de0c6
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-06-26 02:02:44 +0530

    Update Admin_Controller.java

--- src/main/java/edu/icet/ecom/controller/Admin_Controller.java
+++ src/main/java/edu/icet/ecom/controller/Admin_Controller.java
@@ -9,49 +9,44 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/admin")
 @CrossOrigin()
-
 public class Admin_Controller {
+
     private final AdminService adminService;
 
     @GetMapping("/all")
-    public List<AdminDto>getAllAdmins(){
+    public List<AdminDto> getAllAdmins() {
         return adminService.getAllAdmins();
     }
 
     @PostMapping("/add")
-    public ResponseEntity<String> AddAdmin(@RequestBody AdminDto adminDto){
+    public ResponseEntity<String> AddAdmin(@RequestBody AdminDto adminDto) {
         adminService.AddAdmin(adminDto);
         return ResponseEntity.ok("Admin added successfully");
-
     }
+
     @PutMapping("/update/{id}")
-        public ResponseEntity<String> UpdateAdmin(@PathVariable Integer id ,@RequestBody AdminDto adminDto){
-            adminDto.setAdminId(id);
-            adminService.UpdateAdmin(adminDto);
-            return ResponseEntity.ok("Admin updated successfully");
-        }
+    public ResponseEntity<String> UpdateAdmin(@PathVariable Integer id, @RequestBody AdminDto adminDto) {
+        adminDto.setAdminId(id);
+        adminService.UpdateAdmin(adminDto);
+        return ResponseEntity.ok("Admin updated successfully");
+    }
 
     @DeleteMapping("/delete/{id}")
-        public ResponseEntity<String> DeleteAdmin(@PathVariable Integer id) {
+    public ResponseEntity<String> DeleteAdmin(@PathVariable Integer id) {
         adminService.DeleteAdmin(id);
         return ResponseEntity.ok("Admin deleted successfully");
     }
+
     @PostMapping("/login")
     public ResponseEntity<AdminDto> login(@RequestBody LoginRequestDto request) {
-
         AdminDto admin = adminService.login(
                 request.getUsername(),
                 request.getPassword()
         );
-
         return ResponseEntity.ok(admin);
     }
-
-
 }
