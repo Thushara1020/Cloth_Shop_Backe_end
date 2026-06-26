34ce254edff6fd8f08918208b892a8ee19feb9cb
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-06-26 17:25:12 +0530

    Update CustomerService.java

--- src/main/java/edu/icet/ecom/service/CustomerService.java
+++ src/main/java/edu/icet/ecom/service/CustomerService.java
@@ -1,6 +1,7 @@
 package edu.icet.ecom.service;
 
 import edu.icet.ecom.model.dto.CustomerDto;
+import edu.icet.ecom.model.dto.CreditSettlementDto;
 import java.util.List;
 
 public interface CustomerService {
@@ -9,4 +10,7 @@ public interface CustomerService {
     CustomerDto getCustomerById(Long id);
     void updateCustomer(CustomerDto customerDto);
     void deleteCustomer(Long id);
+
+    void settleCredit(CreditSettlementDto settlementDto);
+    List<CreditSettlementDto> getSettlementHistory(Long customerId);
 }
